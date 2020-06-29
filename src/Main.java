56dc769f6f67840a5ceed610d247f7954438a3c9
liziy <https://github.com/lzy9876/SSM-Maven-MySQL.git>
2020-06-29 15:07:55 +0800

    Git测试 第二次提交

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public void demo01(){
+        System.out.println("hello java");
+    }
 }
